d4140c49dc5b21b721fa4b4bc9c46cd55fad0374
Tsimpson000 <Tsimpson000@users.noreply.github.com>
2023-10-31 22:30:37 -0700

    Updated calculator

--- Working_Class_Demo/Calculator.cs
+++ Working_Class_Demo/Calculator.cs
@@ -17,5 +17,9 @@ namespace Working_Class_Demo
         {
             return num1 * num2;
         }
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
     }
 }
